9661fe0894038450c726a67b2ba295ea5ac1c603
JoseBonoko <jbonoko.jb@gmail.com>
2021-05-06 16:10:19 +0200

    ejemplo 1

---
+++ P1/S02/Ejemplos/ej-01-hello.js
@@ -0,0 +1,8 @@
+//-- Programa Hola mundo en Node.js
+
+/* Este es un ejemplo de comentario multilinea  
+   El objeto console está disponible directamente
+   desde node.js, sin tener que importar nada */
+
+//-- Imprimir un mensaje en la consola
+console.log("¡Hola mundo!");
